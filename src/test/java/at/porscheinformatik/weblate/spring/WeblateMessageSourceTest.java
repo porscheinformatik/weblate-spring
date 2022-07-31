4304a753f33374f3eee6b02c6cf254cf1c240a1d
Christian Köberl <christian.koeberl@porscheinformatik.com>
2022-07-31 08:52:43 +0200

    feat: swtich to units API (from file API)
    The weblate API to get translation files performs a commit in the Git
    repository which is usually not intended when fetching the translation
    strings. That's why we change to the translation units API.
    
    The units API also provides the possibility to only load strings that were
    changed since a given timestamp. This helps in reducing the load to the
    server when filling the cache.
    
    Closes #38

--- src/test/java/at/porscheinformatik/weblate/spring/WeblateMessageSourceTest.java
+++ src/test/java/at/porscheinformatik/weblate/spring/WeblateMessageSourceTest.java
@@ -1,5 +1,7 @@
 package at.porscheinformatik.weblate.spring;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
@@ -10,7 +12,6 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.response.DefaultResponseCreator;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Locale;
 import java.util.Properties;
@@ -22,6 +23,13 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 
 class WeblateMessageSourceTest {
+
+  private static final String RESPONSE_OK = "{\"count\":2,\"next\":null,\"previous\":null,\"results\":["
+      + "{\"id\":1,\"context\":\"key1\",\"source\":[\"Hello, World!\"],\"target\":[\"Hello, World!\"]},"
+      + "{\"id\":2,\"context\":\"key2\",\"source\":[\"Wow this works\"],\"target\":[\"Wow this works\"]}"
+      + "]}";
+  private static final String RESPONSE_PAGING = "{\"count\":2,\"next\":\"http://localhost:8080/api/translations/test-project/test-comp/en/units/?page=1&q=state%3A%3E%3Dtranslated\",\"previous\":null,\"results\":[]}";
+  private static final String RESPONSE_EMPTY = "{\"count\":0,\"next\":null,\"previous\":null,\"results\":[]}";
 
   private RestTemplate restTemplate = new RestTemplate();
 
@@ -40,6 +48,11 @@ class WeblateMessageSourceTest {
     mockServer = MockRestServiceServer.createServer(restTemplate);
   }
 
+  @AfterEach
+  void tearDown() {
+    mockServer.verify();
+  }
+
   private void mockGetLocales() {
     mockServer.expect(ExpectedCount.once(),
       requestTo("http://localhost:8080/api/projects/test-project/languages/")
@@ -52,13 +65,13 @@ class WeblateMessageSourceTest {
 
   private void mockResponse(String body) {
     try {
-      URI uri = new URI("http://localhost:8080/api/translations/test-project/test-comp/en/file/?q=state%3A%3E%3Dtranslated");
-      DefaultResponseCreator response = withStatus(HttpStatus.OK).contentType(MediaType.TEXT_PLAIN);
+      String url = "http://localhost:8080/api/translations/test-project/test-comp/en/units/";
+      DefaultResponseCreator response = withStatus(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON);
       if (body != null) {
         response.body(body);
-      }      
+      }
       mockServer.expect(ExpectedCount.once(),
-          requestTo(uri))
+          requestTo(Matchers.startsWith(url)))
           .andExpect(method(HttpMethod.GET))
           .andRespond(response);
     } catch (Exception e) {
@@ -69,18 +82,21 @@ class WeblateMessageSourceTest {
   @Test
   void simpleCase() {
     mockGetLocales();
-    mockResponse("key1=Hello, World!\nkey2=Wow this works");
+    mockResponse(RESPONSE_OK);
+    mockResponse(RESPONSE_OK);
 
-    String key1Value = messageSource.getAllProperties(Locale.ENGLISH).getProperty("key1");
-
+    String key1Value = messageSource.resolveCodeWithoutArguments("key1", Locale.ENGLISH);
     assertEquals("Hello, World!", key1Value);
-    mockServer.verify();
+    key1Value = messageSource.resolveCodeWithoutArguments("key1", Locale.US);
+    assertEquals("Hello, World!", key1Value);
+    key1Value = messageSource.resolveCodeWithoutArguments("key1", Locale.US);
+    assertEquals("Hello, World!", key1Value);
   }
 
   @Test
   void emptyResponse() throws URISyntaxException {
     mockGetLocales();
-    mockResponse(null);
+    mockResponse(RESPONSE_EMPTY);
 
     String key1Value = messageSource.getAllProperties(Locale.ENGLISH).getProperty("key1");
 
@@ -91,15 +107,40 @@ class WeblateMessageSourceTest {
   @Test
   void clearCache() {
     mockGetLocales();
-    mockResponse("key1=Hello, World!\nkey2=Wow this works");
+    mockResponse(RESPONSE_OK);
     mockGetLocales();
-    mockResponse("key1=Another one\nkey2=Wow this works");
+    mockResponse(RESPONSE_OK.replace("Hello, World!", "Another one"));
 
     Properties allProperties = messageSource.getAllProperties(Locale.ENGLISH);
     assertEquals("Hello, World!", allProperties.get("key1"));
     messageSource.clearCache();
     allProperties = messageSource.getAllProperties(Locale.ENGLISH);
     assertEquals("Another one", allProperties.get("key1"));
+  }
+
+  @Test
+  void paging() {
+    mockGetLocales();
+    mockResponse(RESPONSE_PAGING);
+    mockResponse(RESPONSE_PAGING); // try a second paging call
+    mockResponse(RESPONSE_OK);
+    
+    Properties allProperties = messageSource.getAllProperties(Locale.ENGLISH);
+    assertEquals("Hello, World!", allProperties.get("key1"));
+  }
+
+  @Test
+  void cacheTimeout() throws Exception {
+    mockGetLocales();
+    mockResponse(RESPONSE_OK);
+    mockResponse(RESPONSE_OK.replace("Hello, World!", "Another one"));
+
+    assertEquals("Hello, World!", messageSource.resolveCodeWithoutArguments("key1", Locale.ENGLISH));
+    assertEquals("Hello, World!", messageSource.resolveCodeWithoutArguments("key1", Locale.ENGLISH));
+    
+    messageSource.setMaxAgeMilis(1000);
+    Thread.sleep(1001);
+    assertEquals("Another one", messageSource.resolveCodeWithoutArguments("key1", Locale.ENGLISH));
   }
 
 }
